0cbfe6dee741fe54fca37f9cee216b2cfd2a0a70
purplenimbus <you@example.com>
2017-12-16 03:33:56 +0000

    use other service provider for jwt ath

--- config/app.php
+++ config/app.php
@@ -176,8 +176,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-		//Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
-		Tymon\JWTAuth\Providers\LaravelServiceProvider::class
+		Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
+		//Tymon\JWTAuth\Providers\LaravelServiceProvider::class
 
     ],
 
